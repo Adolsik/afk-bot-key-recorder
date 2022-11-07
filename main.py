3afb76c72ac70d4d1e1cd53086033346836d32b1
Adrian Uryga <75134645+Adolsik@users.noreply.github.com>
2022-11-07 18:42:57 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,32 +2,19 @@ import pyautogui as pag
 import time
 import random
 
-time_delayed = 0.0
-# Primary monitor size
-screen_width, screen_high = pag.size()
 
-while True:
-    try:
-        bot_working_time = float(input("Enter how long the bot should run (in minutes):")) * 60
-        while time_delayed <= bot_working_time:
-            # Mouse move
-            pag.click()
-            x = random.randint(0, screen_width)
-            y = random.randint(0, screen_high)
-            pag.moveTo(x, y, 0.5)
-            # Keyboard move
-            pag.press(['w', 's', 'a', 'd', 'space'])
-            time_delayed += 3
-            # interval
-            print("BOT IS WORKING")
-            time.sleep(3)
-        print("BOT HAS FINISHED HIS WORK")
-        break;
-    except:
-        print("Invalid input")
-        time.sleep(1)
+def go(working_time):
+    time_delayed = 0.0
+    # Primary monitor size
+    screen_width, screen_high = pag.size()
+    while time_delayed <= working_time:
+        # Mouse move
+        pag.click()
+        x = random.randint(0, screen_width)
+        y = random.randint(0, screen_high)
+        pag.moveTo(x, y, 0.5)
 
-
-
-
-
+        # Keyboard move
+        pag.press(['w', 's', 'a', 'd', 'space'])
+        time_delayed += 3
+        time.sleep(3)
